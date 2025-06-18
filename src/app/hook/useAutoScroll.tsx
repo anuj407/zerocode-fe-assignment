105f027a55229a8d69cf5a9d7a1bb07c753c1525
anuj407 <anuj97953a@gmail.com>
2025-06-18 18:57:26 +0530

    refactor: simplify useAutoScroll hook by removing generic type and adjusting ref type

--- src/app/hook/useAutoScroll.tsx
+++ src/app/hook/useAutoScroll.tsx
@@ -1,14 +1,13 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-// hooks/useAutoScroll.ts
 import { useEffect, RefObject } from "react";
 
-export default function useAutoScroll<T extends HTMLElement = HTMLDivElement>(
-  ref: RefObject<T>,
+export default function useAutoScroll(
+  ref: RefObject<HTMLElement | null>,
   deps: React.DependencyList = []
 ) {
   useEffect(() => {
     if (ref.current) {
       ref.current.scrollIntoView({ behavior: "smooth" });
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
 }
