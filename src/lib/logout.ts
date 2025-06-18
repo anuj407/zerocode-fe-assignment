c885407cb0730cfee57e5e69a233bdea126b64fe
anuj407 <anuj97953a@gmail.com>
2025-06-18 16:19:46 +0530

    fix: ensure userId and email are removed from local storage on logout

--- src/lib/logout.ts
+++ src/lib/logout.ts
@@ -2,6 +2,8 @@ import toast from "react-hot-toast";
 
 export  const Logout = () => {
   localStorage.removeItem("token");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("email");
   toast.success("Logged out successfully");
   window.location.href = "/login";
 };
